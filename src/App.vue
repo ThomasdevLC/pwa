8d36f546561b3955096d694be0dc682e334cc080
ThomasdevLC <thomas.lecam@gmail.com>
2023-03-03 12:01:41 +0100

    clean

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
-
   <div>
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
-
             <!-- LIST DES ITEMS -->
-            <li class="nav-item" v-for="route in routes">
+            <li class="nav-item" v-for="route in routes" :key="route.title">
               <a class="nav-link" @click="goto(route)">{{ route.title }}</a>
             </li>
             <!-- LIST DES ITEMS -->
-
           </ul>
         </div>
       </div>
     </nav>
 
-    <RouterView/>
-
+    <RouterView />
   </div>
-
 </template>
 
 <script>
-
 export default {
   computed: {
     routes() {
@@ -55,26 +55,23 @@ export default {
           path: "/pres",
           icon: "",
           visible: true,
-        }
+        },
       ];
     },
   },
   data() {
     return {
-      selectedItem : 'home'
-    }
+      selectedItem: "home",
+    };
   },
   methods: {
     goto(route) {
-      console.log('ROUTE', route)
+      console.log("ROUTE", route);
       this.$router.push(route.path);
       /** todo : ajouter la class nav et active */
     },
   },
-}
-
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/routerApp.js
+++ src/routerApp.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AppHome from './pages/AppHome.vue'
-import AppStat from './pages/AppStat.vue'
-import AppPres from './pages/AppPres.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AppHome from "./pages/AppHome.vue";
+import AppStat from "./pages/AppStat.vue";
+import AppPres from "./pages/AppPres.vue";
 
 const routerApp = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: AppHome
-        },
-        {
-            path: '/stat',
-            name: 'stat',
-            component: AppStat
-        },
-        {
-            path: '/pres',
-            name: 'pres',
-            component: AppPres
-        },
-    ]
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: AppHome,
+    },
+    {
+      path: "/stat",
+      name: "stat",
+      component: AppStat,
+    },
+    {
+      path: "/pres",
+      name: "pres",
+      component: AppPres,
+    },
+  ],
+});
 
-export default routerApp
+export default routerApp;
