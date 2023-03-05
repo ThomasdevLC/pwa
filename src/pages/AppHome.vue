3b7dc27369312ea833fb7a19e644bd5fe4fa25ce
ThomasdevLC <thomas.lecam@gmail.com>
2023-03-05 17:39:07 +0100

    get d

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -2,10 +2,32 @@
   <h1>Home</h1>
 
   <h2>{{ post.fullName }}</h2>
+
+  <!-- TOTAL  -->
+
+  <p>TOTAL</p>
+  <p>{{ post.month.stat.nb_total }}</p>
+  <p>{{ post.month.stat.tx_pres_fm }} %</p>
+  <p>{{ post.month.stat.tx_fm }} %</p>
+  <p>{{ post.month.stat.tx_ce }} %</p>
+
+  <!-- VN  -->
+  <p>VN</p>
+  <p>{{ post.month.stat.nb_vn }}</p>
+  <p>{{ post.month.stat.tx_pres_fm_vn }} %</p>
+  <p>{{ post.month.stat.tx_fm_vn }} %</p>
+  <p>{{ post.month.stat.tx_ce_vn }} %</p>
+
+  <!-- VO  -->
+  <p>VO</p>
+  <p>{{ post.month.stat.nb_vo }}</p>
+  <p>{{ post.month.stat.tx_pres_fm_vo }} %</p>
+  <p>{{ post.month.stat.tx_fm_vo }} %</p>
+  <p>{{ post.month.stat.tx_ce_vo }} %</p>
 </template>
 
 <script>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import getTest from "../modules/api";
 
 export default {
