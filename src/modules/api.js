df24c39fa1845a27530bc14219847a17569cf9c2
ThomasdevLC <thomas.lecam@gmail.com>
2023-03-06 14:45:13 +0100

    new call api ok

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,25 +1,14 @@
 import axios from "axios";
-import { ref } from "vue";
 
-const getDatas = (year, month, id) => {
-  const stats = ref([]);
-  const error = ref(null);
-
-  const statDatas = async () => {
-    try {
-      let resp = await axios.get(
-        `https://test.gstat.fr/api/pwa/vendorStat/${year}/${month}/${id}`
-      );
-      console.log(resp.data);
-
-      stats.value = await resp.data;
-    } catch (err) {
-      console.error(err);
-      error.value = err.message;
-    }
-  };
-
-  return { stats, error, statDatas };
+const getData = async (year, month, id) => {
+  try {
+    const response = await axios.get(
+      `     https://test.gstat.fr/api/pwa/vendorStat/${year}/${month}/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export default getDatas;
+export default getData;
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,16 +1,8 @@
 <template>
   <h1>Home</h1>
-  <label>Vendeur:</label>
+  <!-- <label>Vendeur:</label>
   <input v-model="id" type="text" required />
-  <select id="year" name="year" v-model="year" @change="handleRequest">
-    <option>Année</option>
-    <option value="2018">2018</option>
-    <option value="2019">2019</option>
-    <option value="2020">2020</option>
-    <option value="2021">2021</option>
-    <option value="2022">2022</option>
-    <option value="2023">2023</option>
-  </select>
+
   <select id="month" name="month" v-model="month" @change="handleRequest">
     <option>Mois</option>
     <option value="1">Janvier</option>
@@ -25,59 +17,52 @@
     <option value="10">Octobre</option>
     <option value="11">Novembre</option>
     <option value="12">Décembre</option>
-  </select>
+  </select> -->
 
-  <div v-if="stats">
-    <h2>{{ stats.fullName }}</h2>
+  <div v-if="stat">
+    <h2>{{ stat.fullName }}</h2>
     <!-- TOTAL  -->
 
     <p>TOTAL</p>
-    <p>{{ stats.month.stat.nb_total }}</p>
-    <p>{{ stats.month.stat.tx_pres_fm }} %</p>
-    <p>{{ stats.month.stat.tx_fm }} %</p>
-    <p>{{ stats.month.stat.tx_ce }} %</p>
+    <p>{{ stat.nb_total }}</p>
+    <p>{{ stat.tx_pres_fm }} %</p>
+    <p>{{ stat.tx_fm }} %</p>
+    <p>{{ stat.tx_ce }} %</p>
 
     <!-- VN  -->
     <p>VN</p>
-    <p>{{ stats.month.stat.nb_vn }}</p>
-    <p>{{ stats.month.stat.tx_pres_fm_vn }} %</p>
-    <p>{{ stats.month.stat.tx_fm_vn }} %</p>
-    <p>{{ stats.month.stat.tx_ce_vn }} %</p>
+    <p>{{ stat.nb_vn }}</p>
+    <p>{{ stat.tx_pres_fm_vn }} %</p>
+    <p>{{ stat.tx_fm_vn }} %</p>
+    <p>{{ stat.tx_ce_vn }} %</p>
 
     <!-- VO  -->
     <p>VO</p>
-    <p>{{ stats.month.stat.nb_vo }}</p>
-    <p>{{ stats.month.stat.tx_pres_fm_vo }} %</p>
-    <p>{{ stats.month.stat.tx_fm_vo }} %</p>
-    <p>{{ stats.month.stat.tx_ce_vo }} %</p>
+    <p>{{ stat.nb_vo }}</p>
+    <p>{{ stat.tx_pres_fm_vo }} %</p>
+    <p>{{ stat.tx_fm_vo }} %</p>
+    <p>{{ stat.tx_ce_vo }} %</p>
   </div>
 </template>
 
-<script>
-import { ref } from "vue";
-import getDatas from "../modules/api";
+<script setup>
+import { ref, onMounted } from "vue";
+import getData from "../modules/api";
 
-export default {
-  setup() {
-    const id = ref("1283");
-    const year = ref("2023");
-    const month = ref("2");
+onMounted(() => {
+  getStats();
+});
 
-    const { statDatas, stats, error } = getDatas(
-      year.value,
-      month.value,
-      id.value
-    );
+const stat = ref(null);
+const id = ref("1283");
+const year = ref("2023");
+const month = ref("2");
 
-    statDatas();
-
-    const handleRequest = async () => {
-      await statDatas();
-    };
-
-    return { stats, error, id, year, month, handleRequest };
-  },
-};
+function getStats() {
+  getData(year.value, month.value, id.value).then((res) => {
+    stat.value = res.month.stat;
+  });
+}
 </script>
 
 <style></style>
