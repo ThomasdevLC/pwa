4cf80c2e19368fe6d4073bdf4b7cd32758efd017
ThomasdevLC <thomas.lecam@gmail.com>
2023-03-07 11:18:08 +0100

    chart js install

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -34,20 +34,11 @@
     </select>
     <div v-if="error">{{ error }}</div>
   </div>
+  <div class="thing">
+    <div style="width: 90%"><canvas id="myChart"></canvas></div>
+  </div>
 
   <div v-if="stat">
-    <!-- TOTAL  -->
-
-    <!-- <div class="numbers">
-      <div v-if="stat.nb_total">
-        <h3>TOTAL</h3>
-        <p>VN{{ stat.nb_total }}</p>
-        <p>{{ stat.tx_pres_fm }} %</p>
-        <p>{{ stat.tx_fm }} %</p>
-        <p>{{ stat.tx_ce }} %</p>
-      </div>
-    </div> -->
-
     <div class="numbers">
       <!-- VN  -->
       <div v-if="stat.nb_vn">
@@ -73,12 +64,13 @@
 </template>
 
 <script setup>
+import Chart from "chart.js/auto";
 import { ref, onMounted } from "vue";
 import getData from "../modules/api";
 
 const name = ref(null);
 const stat = ref(null);
-const id = ref("");
+const id = ref("1285");
 const months = ref({
   1: "Janvier",
   2: "Février",
@@ -118,9 +110,50 @@ const getStats = async () => {
   }
 };
 
-// onMounted(() => {
-//   getStats();
-// });
+onMounted(() => {
+  getStats();
+});
+
+onMounted(() => {
+  const ctx = document.getElementById("myChart");
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+  myChart;
+});
 
 const handleRequest = async () => {
   getStats();
