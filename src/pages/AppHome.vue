230f7f841f59efee2969a1eb0f5878e5bca8255a
ThomasdevLC <thomas.lecam@gmail.com>
2023-03-07 19:38:59 +0100

    config chart

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -89,11 +89,44 @@ const selectedMonth = ref(new Date().getMonth());
 const years = ref(["2023", "2022", "2021", "2020"]);
 const selectedYear = ref(new Date().getFullYear());
 const error = ref(null);
-
+const myChart = ref(null);
+const myChartConfig = ref({
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [10, 20, 30, 0, 0, 0],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
 const chartVn = ref();
-
-console.log("chart", chartVn);
-console.log("stat", stat);
 
 const getStats = async () => {
   try {
@@ -111,7 +144,18 @@ const getStats = async () => {
     ];
     error.value = null;
     console.log("test home", res);
-    return chartVn;
+
+    myChartConfig.value.data.datasets[0].data = [
+      res.month.stat.tx_pres_fm_vn,
+      res.month.stat.tx_fm_vn,
+      res.month.stat.tx_ce_vn,
+      20,
+      20,
+      20,
+    ];
+    console.log(myChartConfig.value.data.datasets[0].data);
+
+    // myChart.update();
   } catch (err) {
     console.log("err home", err);
     error.value = err.message;
@@ -120,46 +164,10 @@ const getStats = async () => {
 };
 
 onMounted(() => {
-  getStats();
-
   const ctx = document.getElementById("myChart");
-  const myChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-  myChart;
+  myChart.value = new Chart(ctx, myChartConfig.value);
+
+  getStats();
 });
 
 const handleRequest = async () => {
