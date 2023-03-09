8d914b4bd041acbf8cd5858b517eda2d5c0be492
ThomasdevLC <thomas.lecam@gmail.com>
2023-03-09 14:14:55 +0100

    affichage store +vn/vo

--- src/pages/AppHome2.vue
+++ src/pages/AppHome2.vue
@@ -1,147 +1,157 @@
 <template>
-  <div v-if="name">
-    <h2>{{ name }}</h2>
-  </div>
+  <div v-if="!data">LOADER</div>
 
-  <div class="search">
-    <input
-      v-model="id"
-      type="text"
-      @keydown.enter.prevent="handleRequest"
-      required
-    />
+  <div v-else>
+    <div>
+      <h2>{{ data.fullName }} {{ data.typeToString }}</h2>
+      <h2>{{ data.storeToString }}</h2>
+    </div>
 
-    <select
-      id="month"
-      name="month"
-      v-model="selectedMonth"
-      @change="handleRequest"
-    >
-      <option v-for="(month, index) in months" :value="index" :key="index">
-        {{ month }}
-      </option>
-    </select>
+    <div class="search">
+      <input
+        v-model="id"
+        type="text"
+        @keydown.enter.prevent="handleRequest"
+        required
+      />
 
-    <select
-      id="year"
-      name="year"
-      v-model="selectedYear"
-      @change="handleRequest"
-    >
-      <option v-for="year in years" :value="year" :key="year">
-        {{ year }}
-      </option>
-    </select>
-    <div v-if="error">{{ error }}</div>
-  </div>
+      <select
+        id="month"
+        name="month"
+        v-model="selectedMonth"
+        @change="handleRequest"
+      >
+        <option v-for="(month, index) in months" :value="index" :key="index">
+          {{ month }}
+        </option>
+      </select>
 
-  <div v-if="stat">
-    <div class="numbers">
-      <!-- VN  -->
-      <div v-if="stat.nb_vn">
-        <h3>VN</h3>
+      <select
+        id="year"
+        name="year"
+        v-model="selectedYear"
+        @change="handleRequest"
+      >
+        <option v-for="year in years" :value="year" :key="year">
+          {{ year }}
+        </option>
+      </select>
+      <div v-if="error">{{ error }}</div>
+    </div>
 
-        <p>Total : {{ stat.nb_vn }}</p>
+    <div v-if="stat">
+      <div class="radial">
         <div
           class="pie animate no-round"
           :style="{ '--p': percentage.value, '--c': 'orange' }"
         >
           {{ percentage.value }}%
         </div>
-
-        <table
-          class="charts-css bar show-labels data-spacing-5 labels-align-start show-data-axes"
-        >
-          <tbody>
-            <tr>
-              <th scope="row">Pré FM :</th>
-              <td
-                :style="
-                  '--size: calc(' +
-                  stat.tx_pres_fm_vn +
-                  '  / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_pres_fm_vn }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Pén FM :</th>
-
-              <td
-                :style="
-                  '--size: calc(' + stat.tx_fm_vn + ' / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_fm_vn }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Péné CE :</th>
-
-              <td
-                :style="
-                  '--size: calc(' + stat.tx_ce_vn + ' / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_ce_vn }} </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-    </div>
+      <div class="numbers">
+        <!-- VN  -->
+        <div v-if="stat.nb_vn">
+          <h3>VN : {{ stat.nb_vn }}</h3>
 
-    <div class="numbers">
-      <!-- VO  -->
-      <div v-if="stat.nb_vo">
-        <h3>VO</h3>
-        <p>Total : {{ stat.nb_vo }}</p>
+          <table
+            class="charts-css bar show-labels data-spacing-5 labels-align-start show-data-axes"
+          >
+            <tbody>
+              <tr>
+                <th scope="row">Pré FM :</th>
+                <td
+                  :style="
+                    '--size: calc(' +
+                    stat.tx_pres_fm_vn +
+                    '  / 100 ); --color: orange;'
+                  "
+                >
+                  <span class="data"> {{ stat.tx_pres_fm_vn }} </span>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Pén FM :</th>
 
-        <table
-          id="my-chart"
-          class="charts-css bar show-labels labels-align-start data-spacing-5 labels-align-start show-data-axes"
-        >
-          <tbody>
-            <tr>
-              <th class="title" scope="row">Prés FM %</th>
-              <td
-                :style="
-                  '--size: calc(' +
-                  stat.tx_pres_fm_vo +
-                  '  / 100 ); --color: orange; '
-                "
-              >
-                <span class="data"> {{ stat.tx_pres_fm_vo }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Péné FM %</th>
+                <td
+                  :style="
+                    '--size: calc(' +
+                    stat.tx_fm_vn +
+                    ' / 100 ); --color: orange;'
+                  "
+                >
+                  <span class="data"> {{ stat.tx_fm_vn }} </span>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Péné CE :</th>
 
-              <td
-                :style="
-                  '--size: calc(' + stat.tx_fm_vo + ' / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_fm_vo }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th class="title" scope="row">Péné CE %</th>
-
-              <td
-                :style="
-                  ';--size: calc(' +
-                  stat.tx_ce_vo +
-                  ' / 100 ); --color: orange ; '
-                "
-              >
-                <span class="data"> {{ stat.tx_ce_vo }} </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                <td
+                  :style="
+                    '--size: calc(' +
+                    stat.tx_ce_vn +
+                    ' / 100 ); --color: orange;'
+                  "
+                >
+                  <span class="data"> {{ stat.tx_ce_vn }} </span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
+
+      <div class="numbers">
+        <!-- VO  -->
+        <div v-if="stat.nb_vo">
+          <h3>VO : {{ stat.nb_vo }}</h3>
+          <table
+            id="my-chart"
+            class="charts-css bar show-labels labels-align-start data-spacing-5 labels-align-start show-data-axes"
+          >
+            <tbody>
+              <tr>
+                <th class="title" scope="row">Prés FM %</th>
+                <td
+                  :style="
+                    '--size: calc(' +
+                    stat.tx_pres_fm_vo +
+                    '  / 100 ); --color: orange; '
+                  "
+                >
+                  <span class="data"> {{ stat.tx_pres_fm_vo }} </span>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Péné FM %</th>
+
+                <td
+                  :style="
+                    '--size: calc(' +
+                    stat.tx_fm_vo +
+                    ' / 100 ); --color: orange;'
+                  "
+                >
+                  <span class="data"> {{ stat.tx_fm_vo }} </span>
+                </td>
+              </tr>
+              <tr>
+                <th class="title" scope="row">Péné CE %</th>
+
+                <td
+                  :style="
+                    ';--size: calc(' +
+                    stat.tx_ce_vo +
+                    ' / 100 ); --color: orange ; '
+                  "
+                >
+                  <span class="data"> {{ stat.tx_ce_vo }} </span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <pre> {{ data }}  </pre>
     </div>
   </div>
 </template>
@@ -153,7 +163,7 @@ import getData from "../modules/api";
 
 // infos id + date
 const id = ref("1283");
-const name = ref(null);
+const data = ref(null);
 const months = ref({
   1: "Janvier",
   2: "Février",
@@ -186,10 +196,10 @@ const getStats = async () => {
       selectedMonth.value,
       id.value
     );
-    name.value = res.fullName;
+    data.value = res;
     stat.value = res.month.stat;
     objectives.value = res.month.objectives.total;
-    total.value = res.month.stat.nb_vn;
+    total.value = res.month.stat.nb_total;
     error.value = null;
     console.log("test home", res);
   } catch (err) {
@@ -272,6 +282,12 @@ select {
   syntax: "<number>";
   inherits: true;
   initial-value: 0;
+}
+
+.radial {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .pie {
