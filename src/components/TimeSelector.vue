5e57ab710fb18a1c4e571c9bf203304433024478
ThomasdevLC <thomas.lecam@gmail.com>
2023-03-10 15:22:18 +0100

    timeselector importé v1

--- src/components/TimeSelector.vue
+++ src/components/TimeSelector.vue
@@ -28,22 +28,34 @@
         {{ year }}
       </option>
     </select>
-    <div v-if="error">{{ error }}</div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   emits: ["change"],
 
   setup() {
-    const date = {
-      year: "2023",
-      month: "2",
-      id: "1283",
-    };
-
-    return { date };
+    const id = ref("1283");
+    const months = ref({
+      1: "Janvier",
+      2: "Février",
+      3: "Mars",
+      4: "Avril",
+      5: "Mai",
+      6: "Juin",
+      7: "Juillet",
+      8: "Août",
+      9: "Septembre",
+      10: "Octobre",
+      11: "Novembre",
+      12: "Décembre",
+    });
+    const selectedMonth = ref(new Date().getMonth() + 1);
+    const years = ref(["2023", "2022", "2021", "2020"]);
+    const selectedYear = ref(new Date().getFullYear());
+    return { id, months, selectedMonth, years, selectedYear };
   },
 };
 </script>
