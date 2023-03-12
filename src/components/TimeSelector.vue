7d257c4de7817f0c005ea445d36ecaa4eb721991
Alexandre LE CAM <a.lecam@groupegca.com>
2023-03-12 17:12:05 +0100

    compositionToOptions

--- src/components/TimeSelector.vue
+++ src/components/TimeSelector.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="search">
     <input
-      v-model="date.id"
-      type="text"
-      @keydown.enter.prevent="$emit('change', date)"
-      required
+        v-model="date.id"
+        type="text"
+        @keydown.enter.prevent="changeDate"
+        required
     />
 
     <select
-      id="month"
-      name="month"
-      v-model="date.month"
-      @change="$emit('change', date)"
+        id="month"
+        name="month"
+        v-model="date.month"
+        @change="changeDate"
     >
       <option v-for="(month, index) in months" :value="index" :key="index">
         {{ month }}
@@ -19,10 +19,10 @@
     </select>
 
     <select
-      id="year"
-      name="year"
-      v-model="date.year"
-      @change="$emit('change', date)"
+        id="year"
+        name="year"
+        v-model="date.year"
+        @change="changeDate"
     >
       <option v-for="year in years" :value="year" :key="year">
         {{ year }}
@@ -32,39 +32,41 @@
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
 export default {
-  emits: ["change"],
-
-  setup() {
-    const id = ref("1283");
-    const months = ref({
-      1: "Janvier",
-      2: "Février",
-      3: "Mars",
-      4: "Avril",
-      5: "Mai",
-      6: "Juin",
-      7: "Juillet",
-      8: "Août",
-      9: "Septembre",
-      10: "Octobre",
-      11: "Novembre",
-      12: "Décembre",
-    });
-
-    const years = ref(["2023", "2022", "2021", "2020"]);
-
-    const date = ref({
-      year: new Date().getFullYear(),
-      month: new Date().getMonth() + 1,
-      id: "1283",
-    });
-
-    onMounted(() => {});
-
-    return { id, months, years, date };
+  mounted() {
+    this.$emit('date-change', this.date)
   },
-};
+  data() {
+    return {
+      months: {
+        'year': "Annuel",
+        1: "Janvier",
+        2: "Février",
+        3: "Mars",
+        4: "Avril",
+        5: "Mai",
+        6: "Juin",
+        7: "Juillet",
+        8: "Août",
+        9: "Septembre",
+        10: "Octobre",
+        11: "Novembre",
+        12: "Décembre",
+      },
+      years: ["2023", "2022", "2021", "2020"],
+      date: {
+        year: new Date().getFullYear(),
+        month: new Date().getMonth() + 1,
+        id: "1283",
+      },
+    }
+  },
+  methods: {
+    changeDate() {
+      this.$emit('date-change', this.date)
+    }
+  },
+}
 </script>
-<style></style>
+
+
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,177 +1,86 @@
 <template>
-  <div v-if="!data">LOADER</div>
-
-  <div v-else>
-    <div>
-      <h2>{{ data.fullName }} {{ data.typeToString }} Annuel</h2>
-      <h2>{{ data.storeToString }}</h2>
-    </div>
-
-    <div class="search">
-      <input
-        v-model="id"
-        type="text"
-        @keydown.enter.prevent="handleRequest"
-        required
-      />
-
-      <select
-        id="year"
-        name="year"
-        v-model="selectedYear"
-        @change="handleRequest"
-      >
-        <option v-for="year in years" :value="year" :key="year">
-          {{ year }}
-        </option>
-      </select>
-      <div v-if="error">{{ error }}</div>
-    </div>
-
-    <div class="numbers">
-      <!-- TOTAL  -->
-
-      <h3>TOTAL : {{ stat.nb_total }}</h3>
-
-      <!-- VN  -->
-      <div v-if="stat.nb_vn">
-        <h3>VN : {{ stat.nb_vn }}</h3>
-
-        <table
-          class="charts-css bar show-labels data-spacing-5 labels-align-start show-data-axes"
-        >
-          <tbody>
-            <tr>
-              <th scope="row">Pré FM :</th>
-              <td
-                :style="
-                  '--size: calc(' +
-                  stat.tx_pres_fm_vn +
-                  '  / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_pres_fm_vn }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Pén FM :</th>
-
-              <td
-                :style="
-                  '--size: calc(' + stat.tx_fm_vn + ' / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_fm_vn }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Péné CE :</th>
-
-              <td
-                :style="
-                  '--size: calc(' + stat.tx_ce_vn + ' / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_ce_vn }} </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-
-    <div class="numbers">
-      <!-- VO  -->
-      <div v-if="stat.nb_vo">
-        <h3>VO : {{ stat.nb_vo }}</h3>
-        <table
-          id="my-chart"
-          class="charts-css bar show-labels labels-align-start data-spacing-5 labels-align-start show-data-axes"
-        >
-          <tbody>
-            <tr>
-              <th class="title" scope="row">Prés FM %</th>
-              <td
-                :style="
-                  '--size: calc(' +
-                  stat.tx_pres_fm_vo +
-                  '  / 100 ); --color: orange; '
-                "
-              >
-                <span class="data"> {{ stat.tx_pres_fm_vo }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Péné FM %</th>
-
-              <td
-                :style="
-                  '--size: calc(' + stat.tx_fm_vo + ' / 100 ); --color: orange;'
-                "
-              >
-                <span class="data"> {{ stat.tx_fm_vo }} </span>
-              </td>
-            </tr>
-            <tr>
-              <th class="title" scope="row">Péné CE %</th>
-
-              <td
-                :style="
-                  ';--size: calc(' +
-                  stat.tx_ce_vo +
-                  ' / 100 ); --color: orange ; '
-                "
-              >
-                <span class="data"> {{ stat.tx_ce_vo }} </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <pre> {{ data }}  </pre>
+  <div>
+    <TimeSelector @date-change="getStat" />
   </div>
+
+  <div v-if="data">
+
+
+    <div class="numbers">
+      <ChartsTotal :total="stat.nb_total" :percentage="percentage" />
+      <ChartsRates
+          title="VN"
+          :total="stat.nb_vn"
+          :txPres="stat.tx_pres_fm_vn"
+          :txFm="stat.tx_fm_vn"
+          :txCe="stat.tx_ce_vn"
+      />
+    </div>
+    <div class="numbers">
+      <ChartsRates
+          title="VO"
+          :total="stat.nb_vo"
+          :txPres="stat.tx_pres_fm_vo"
+          :txFm="stat.tx_fm_vo"
+          :txCe="stat.tx_ce_vo"
+      />
+    </div>
+  </div>
+
+  <!-- TEST -->
+<!--  <div>
+    <pre> {{ data }}  </pre>
+
+  </div>-->
+
 </template>
 
-<script setup>
+<script>
 import "charts.css";
-import { ref, onMounted, computed } from "vue";
 import getData from "../modules/api";
+import TimeSelector from "../components/TimeSelector.vue";
+import ChartsTotal from "../components/ChartsTotal.vue";
+import ChartsRates from "../components/ChartsRates.vue";
 
-// infos id + date
-const id = ref("1283");
-const data = ref(null);
-const years = ref(["2023", "2022", "2021", "2020"]);
-const selectedYear = ref(new Date().getFullYear());
-const error = ref(null);
-
-// infos stats
-const stat = ref(null);
-const total = ref(null);
-
-const getStats = async () => {
-  try {
-    const res = await getData(selectedYear.value, "year", id.value);
-    data.value = res;
-    stat.value = res.select.stat;
-    console.log("stat", stat.value);
-    error.value = null;
-
-    console.log("test home", res);
-  } catch (err) {
-    console.log("err home", err);
-    error.value = err.message;
-    stat.value = null;
+export default {
+  components: {TimeSelector, ChartsTotal, ChartsRates},
+  mounted() {
+  },
+  data() {
+    return {
+      data: null,
+      error: null,
+      stat: null,
+      objectives: null,
+      total: null,
+    }
+  },
+  computed: {
+    percentage() {
+      let percentage = Math.round((this.total / this.objectives) * 100);
+      return Math.min(percentage, 100);
+    }
+  },
+  methods: {
+    getStat(date) {
+      getData(date.year, date.month, date.id)
+          .then((res) => {
+            console.log("RES TOTO", res);
+            this.data = res;
+            /** todo : Faire une ternaire */
+            this.stat = res.month.stat;
+            this.objectives = res.month.objectives.total;
+            this.total = res.month.stat.nb_total;
+            this.error = null;
+          })
+          .catch((err) => {
+            console.log("err home", err);
+            this.error = err.message;
+            this.stat = null;
+          });
+    }
   }
-};
-
-onMounted(() => {
-  getStats();
-});
-
-const handleRequest = () => {
-  getStats();
-};
+}
 </script>
 
 <style>
@@ -214,87 +123,10 @@ select {
   background: #ff8800;
   position: absolute;
   z-index: -1;
-  padding: 0 50px;
+  padding-right: 130px;
+  left: -30px;
   transform: rotateZ(-1deg);
 }
-
-#my-chart.bar {
-  --labels-size: 100px;
-}
-
-.data {
-  padding: 0px 5px;
-}
-
-@property --p {
-  syntax: "<number>";
-  inherits: true;
-  initial-value: 0;
-}
-
-.radial {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.pie {
-  --p: 20;
-  --b: 22px;
-  --c: #ff8800;
-  --w: 150px;
-
-  width: var(--w);
-  aspect-ratio: 1;
-  position: relative;
-  display: inline-grid;
-  margin: 5px;
-  place-content: center;
-  font-size: 25px;
-  font-weight: bold;
-  font-family: sans-serif;
-}
-.pie:before,
-.pie:after {
-  content: "";
-  position: absolute;
-  border-radius: 50%;
-}
-.pie:before {
-  inset: 0;
-  background: radial-gradient(farthest-side, var(--c) 98%, #0000) top/var(--b)
-      var(--b) no-repeat,
-    conic-gradient(var(--c) calc(var(--p) * 1%), #0000 0);
-  -webkit-mask: radial-gradient(
-    farthest-side,
-    #0000 calc(99% - var(--b)),
-    #000 calc(100% - var(--b))
-  );
-  mask: radial-gradient(
-    farthest-side,
-    #0000 calc(99% - var(--b)),
-    #000 calc(100% - var(--b))
-  );
-}
-.pie:after {
-  inset: calc(50% - var(--b) / 2);
-  background: var(--c);
-  transform: rotate(calc(var(--p) * 3.6deg))
-    translateY(calc(50% - var(--w) / 2));
-}
-.animate {
-  animation: p 1s 0.5s both;
-}
-.no-round:before {
-  background-size: 0 0, auto;
-  border: 3px solid rgb(211, 198, 198);
-}
-.no-round:after {
-  content: none;
-}
-@keyframes p {
-  from {
-    --p: 0;
-  }
-}
 </style>
+
+
