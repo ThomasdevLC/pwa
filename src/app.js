ea3a09e267753ff13a0d50cf54ff6a8a7ef5a495
ThomasdevLC <thomas.lecam@gmail.com>
2023-10-05 10:42:09 +0200

    clean code

--- src/app.js
+++ src/app.js
@@ -4,11 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./routerApp";
 
-import "./assets/app.css";
 import "./assets/styles/index.scss";
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap";
 
 const app = createApp(App);
 
